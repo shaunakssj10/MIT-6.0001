28d2291d973f107583d44c568f31d437b1b443c1
shaunakssj10 <60471157+shaunakssj10@users.noreply.github.com>
2025-07-12 18:10:00 -0500

    Add files via upload

---
+++ PS0/PS0.py
@@ -0,0 +1,9 @@
+import math
+
+var_a = int(input("Enter number x:"))
+var_b = int(input("Enter number y:"))
+power = (var_a)**(var_b)
+log = math.log2(var_a)
+
+print("x**y = ",power)
+print("log (x) = ", log)
